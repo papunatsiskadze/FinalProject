9a86517ad71288e8f2cc2ab073c88c01a14ba67f
papunatsiskadze <papunaciskadze@gmail.com>
2022-07-08 17:19:03 +0400

    Slider done

--- JAVASCRIPT/SliderJS.js
+++ JAVASCRIPT/SliderJS.js
@@ -5,6 +5,13 @@ let currentSlide = 1;
 // Manual slide code
 
 var manualSlide = function(manual){
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+    bubbles.forEach((bubble) => {
+        bubble.classList.remove('active');
+    });
+});
     slides[manual].classList.add('active');
     bubbles[manual].classList.add('active');
 }
@@ -12,5 +19,33 @@ bubbles.forEach((bubble, i) => {
     bubble.addEventListener("click", () => {
         manualSlide(i);
         currentSlide = i;
-    })
-})
+    });
+});
+
+
+//  Auto Slider
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    i = 1;
+    var repeater = () => {
+        setTimeout(function(){
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active')
+            });
+
+            slides[i].classList.add('active');
+            bubbles[i].classList.add('active');
+            i++;
+
+            if(slides.length == i){
+                i = 0;
+            }
+            if(i >= slides.length){
+                return;
+            }
+            repeater();
+        }, 7000);
+    }
+    repeater();
+}
+repeat();
