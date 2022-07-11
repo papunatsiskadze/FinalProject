0d3c63ddbef425a49d9072aeb04ae8cb9bd2d19c
papunatsiskadze <papunaciskadze@gmail.com>
2022-07-12 02:07:55 +0400

    fully done Movie Api

--- JAVASCRIPT/API.js
+++ JAVASCRIPT/API.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=${movies}&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=%22';
-
+// const STATIC_URL = 'https://api.themoviedb.org/3/'
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
@@ -11,15 +11,21 @@ getMovies(API_URL)
 async function getMovies(url){
     const res= await fetch(url)
     const data= await res.json()
-    console.log(data.results)
     displayMovies(data.results)
 }
 
+// function moviesByType(type){
+//     let api = `https://api.themoviedb.org/3/discover/movie?type=${type}&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';`
+//     console.log(api)
+// }
+
 function displayMovies(movies){
+   
     main.innerHTML=''
-    movies.foreach((movie) => {
+    for (let index = 0; index < movies.length; index++) {
+        let movie = movies[index]
+       
         const { title,poster_path,vote_average,overview} = movie
-        
         const moviesEl = document.createElement('div')
         
         moviesEl.classList.add('movie')
@@ -36,7 +42,8 @@ function displayMovies(movies){
         `
 
         main.appendChild(moviesEl);
-    })
+    }
+
 }
 
 function getClassesByRating(rating){
@@ -48,6 +55,8 @@ function getClassesByRating(rating){
         return 'red'
     }
 }
+
+
 //search pathing 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -59,3 +68,8 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+// let button = document.getElementById('tea')
+// button.addEventListener('submit', (e) => {
+//     console.log(e)
+//     // moviesByType();
+// })
